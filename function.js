30000e6f817759c826b72c1c8494296845891447
Fale1278 <kingnanmuafale@gmail.com>
2026-05-15 03:47:21 -0700

    progress

---
+++ function.js
@@ -0,0 +1,29 @@
+
+// FUNCTIONS
+
+// function funcName(parameter){
+//     statement;
+// }
+
+// funcName();
+
+// function multiply(num1, num2){
+//     console.log(num1 * num2)
+// }
+
+// multiply(45, 56);
+// add(90, 78);
+// add(80, 10);
+
+// function greet(){
+//     return "Hello World";
+// }
+
+// let greeting = greet();
+// console.log(greeting)
+
+const greet = () => {
+    console.log("Hello")
+}
+
+greet()
---
+++ script.js
@@ -0,0 +1,100 @@
+// console.log('"Hello"')
+// console.log(`"I can't come"`)
+
+// TO check the length of a string
+// let text = "This is an introductory class";
+// let length = text.length;
+
+// console.log(length)
+
+// let fisrtname = "King";
+// let age = "23";
+
+// console.log(fisrtname + " is " + age)
+// console.log(`${fisrtname} is ${age}`)
+
+// let text = "Kingshima";
+// // let char = text.charAt(6);
+// let char = text.charCodeAt(6);
+// let upper = text.toUpperCase();
+// console.log(text)
+// console.log(char)
+// console.log(upper)
+
+// let name = "King,is,a,web,developer";
+// console.log(name);
+// console.log(name.trim());
+// console.log(name.includes("java"));
+// console.log(name.startsWith("   "));
+// console.log(name.endsWith("  "));
+// console.log(name.replace("King", "Zion"));
+// console.log(name.slice(0, 8));
+
+// console.log(name.split(","))
+
+// let text = "Kingshima";
+
+// console.
+
+// NUMBER METHODS
+// let x = 23
+// let y = 45
+
+// let z = x + y
+// console.log(z)
+
+// let a = "34";
+// let b = "45";
+
+// let p = a + b;
+// console.log(p)
+
+// toString()
+// let num = 100;
+
+// console.log(num + 100);
+// console.log(typeof num);
+
+// console.log(num.toString() + 100)
+// console.log(typeof num.toString())
+
+// toFixed()
+// let price = 9.5564;
+
+// console.log(price)
+// console.log(price.toFixed())
+// console.log(price.toFixed(2))
+
+// toPrecision()
+
+// let num = 123.445;
+
+// console.log(num)
+// console.log(num.toPrecision(4))
+
+// Number()
+
+// let string = "67984847";
+
+// let newValue = string + 10000
+// console.log(newValue)
+
+// let number = Number(string) + 10000
+// console.log(number)
+
+// isNaN
+
+// console.log(isNaN("100"))
+
+// Math.round()
+
+// console.log(Math.round(4.9))
+// console.log(Math.floor(4.9))
+// console.log(Math.ceil(4.3))
+// console.log(Math.random())
+
+let price = 5000;
+let discount = 0.1;
+let finalPrice = price - (price * discount);
+
+console.log(finalPrice)
